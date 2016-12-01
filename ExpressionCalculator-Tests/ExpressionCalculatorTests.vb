b79127a5afd7fba82a6f3288f4e81883941c62ea
Josh Perry <joshperry@outlook.com>
2016-12-01 16:35:22 +0000

    Expression.Calculate now uses Microsoft Script Control to eval Added a tests project and a simple addition test

---
+++ ExpressionCalculator-Tests/ExpressionCalculatorTests.vb
@@ -0,0 +1,13 @@
+﻿<TestClass()> Public Class ExpressionCalculatorTests
+    <TestMethod()> Public Sub Test_SimpleAddition()
+        ' Arrange
+        Dim expString As String = "2+2"
+        Dim exp As ExpressionCalculator.Expression = New ExpressionCalculator.Expression(expString)
+
+        ' Act
+        Dim result = exp.Calculate()
+
+        ' Assert
+        Assert.AreEqual(result, 4)
+    End Sub
+End Class
--- ExpressionCalculator/Expression.vb
+++ ExpressionCalculator/Expression.vb
@@ -1,12 +1,33 @@
 ﻿Public Class Expression
+    ''' <summary>
+    ''' The original expression in string form
+    ''' </summary>
+    ''' <returns></returns>
     Property Expression As String = String.Empty
 
-    Sub New(exp As String)
-        Expression = exp
+    ''' <summary>
+    ''' Create a new expression from the provided string
+    ''' </summary>
+    ''' <param name="expressionStr">The string to create the expression from</param>
+    Sub New(expressionStr As String)
+        Expression = expressionStr
     End Sub
 
+    ''' <summary>
+    ''' Calculate the expression stored in Expression and return the result
+    ''' </summary>
+    ''' <returns>The result of the calculation</returns>
     Public Function Calculate()
-        Dim result As Integer = 10
+        Dim result As Integer
+
+        Dim scriptControl As New MSScriptControl.ScriptControl()
+        scriptControl.Language = "VBScript"
+
+        Try
+            result = Convert.ToDouble(scriptControl.Eval(Expression))
+        Catch ex As Exception
+
+        End Try
 
         Return result
     End Function
